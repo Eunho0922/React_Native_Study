0f1f7aa53bcef9c7ca1525af83ca646bfad550d0
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-07-09 14:08:03 +0900

    React_Native :: 코어 컴포넌트 - View

--- react_native_practice/App.js
+++ react_native_practice/App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from 'react-native';  
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
-      <View style={{ flex: 1, backgroundColor: 'red'  }}></View>
-      <View style={{ flex: 1, backgroundColor: 'black'  }}></View>
-      <View style={{ flex: 1, backgroundColor: 'yellow'  }}></View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello, World!</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
