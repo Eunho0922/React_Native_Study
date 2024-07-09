9c84c33680686a4c2a446bd6e3d836b38d7e7b49
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-07-09 15:20:00 +0900

    React_Native :: Flexbox 레이아웃

--- react_native_practice/App.js
+++ react_native_practice/App.js
@@ -1,43 +1,26 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-const App = () => {
+const Flex = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Box with margin</Text>
-      </View>
-      <View style={styles.box}>
-        <Text style={styles.textWithPadding}>Box with padding</Text>
-      </View>
+    <View style={[
+      styles.container,
+      {
+        flexDirection: 'column'
+      },
+    ]}>
+      <View style={{ flex: 1, backgroundColor: 'red' }}></View>
+      <View style={{ flex: 2, backgroundColor: 'darkorange' }}></View>
+      <View style={{ flex: 3, backgroundColor: 'green' }}></View>
     </View>
   )
 }
 
-export default App;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0'
-  },
-  box: {
-    width: 200,
-    height: 100,
-    backgroundColor: '#4CAF50',
-    margin: 20,
-  },
-  text: {
-    color: 'white',
-    textAlign: 'center',
-    lineHeight: 100,
-  },
-  textWithPadding: {
-    color: 'white',
-    textAlign: 'center',
     padding: 20,
-    backgroundColor: '#red',
-  }
+  },
 })
+
+export default Flex;
