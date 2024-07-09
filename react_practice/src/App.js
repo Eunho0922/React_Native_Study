04d704e403a025b9135007b3eb6ea5104d3333cc
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-07-09 09:03:50 +0900

    React :: JSX 맛보기 코드

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -2,24 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    return (
-        <div className='App'>
-            <header className='App-header'>
-            <h1>Declarative Programming Example</h1>
-            { isLoggedIn ? (
-              <p>Welcome, User!</p>
-            ) : (
-              <p>Please log in.</p>
-            )}
-            <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
-            {isLoggedIn ? 'Log Out' : 'Log In'}
-            </button>
-            </header>
-        </div>
-    )
+    // return React.createElement(
+    //   "h1",
+    //   { title: "This works!" },
+    //   "Hi, this is a React App!"
+    // );
+    return <h1 title='This wokrs!'>Hi, this is a React App!</h1>
 }
 
 export default App;
-
