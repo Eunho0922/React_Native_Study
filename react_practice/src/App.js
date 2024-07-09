4d2e020c86975eb27ef35b5aceda7c9769f9fa3d
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-07-09 10:48:53 +0900

    React :: 상태 관리 컴포넌트

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,19 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 
 import GoalList from "./componenets/GoalList/GoalList"
 import NewGoal from "./componenets/NewGoal/NewGoal";
 import './App.css';  
 
 function App() { 
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] =  useState([
     { id: "cg1", text: "Finish the Course" },
     { id: "cg2", text: "Learn all about the Course Main Topic" },
     { id: "cg3", text: "Help other students in the Course Q&A" },
-  ];
+  ]);
 
   const addNewGoalsHandler = (newGoal) => {
-    courseGoals.push(newGoal)
-    console.log(courseGoals);
+    setCourseGoals(courseGoals.concat(newGoal));
+
+    // setCourseGoals([...courseGoals, newGoal])
+
+    // setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
+    // setCourseGoals((prevCourseGoals) => [...prevCourseGoals, newGoal])
   }
     return (
       <div className='course-goals'>
