126c01add8508c5881a29063b01e40f07b331e8e
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-07-09 14:58:09 +0900

    React_Native :: margin & padding 레이아웃

--- react_native_practice/App.js
+++ react_native_practice/App.js
@@ -1,45 +1,43 @@
 import React from "react";
-import { SectionList, Text, View } from 'react-native';  
+import { StyleSheet, View, Text } from "react-native";
 
-const DATA = [
-  {
-    title: 'Main dishes',
-    data: ['Pizza', 'Burger', 'Risotto']
-  },
-  {
-    title: 'Sides',
-    data: ['French Fries', 'Onion Rings', 'Fried Shrimps']
-  },
-  {
-    title: 'Sides',
-    data: ['French Fries', 'Onion Rings', 'Fried Shrimps']
-  },
-  {
-    title: 'Sides',
-    data: ['French Fries', 'Onion Rings', 'Fried Shrimps']
-  },
-  {
-    title: 'Sides',
-    data: ['French Fries', 'Onion Rings', 'Fried Shrimps']
-  },
-  {
-    title: 'Sides',
-    data: ['French Fries', 'Onion Rings', 'Fried Shrimps']
-  },
-];
-
-export default function App() {
+const App = () => {
   return (
-    <SectionList 
-      style={{ marginTop: 10 }}
-      sections={DATA}
-      keyExtractor={(item, index) => item + index}
-      renderItem={({item}) => <Text style={{padding: 20}}>{item}</Text>}
-      renderSectionHeader={({ section: { title } }) => (
-        <View style={{ backgroundColor: "#f0f0f0", padding: 10 }}>
-          <Text style={{ fontWeigh: 'bold', fontSize: 18 }}>{title}</Text>
-        </View>
-      )}
-    />
-  );
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>Box with margin</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.textWithPadding}>Box with padding</Text>
+      </View>
+    </View>
+  )
 }
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0'
+  },
+  box: {
+    width: 200,
+    height: 100,
+    backgroundColor: '#4CAF50',
+    margin: 20,
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    lineHeight: 100,
+  },
+  textWithPadding: {
+    color: 'white',
+    textAlign: 'center',
+    padding: 20,
+    backgroundColor: '#red',
+  }
+})
