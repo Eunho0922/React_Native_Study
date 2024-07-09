d575b32a62f0379a0dd1e0b192ecab5863816715
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-07-09 14:19:11 +0900

    React_Native :: 코어 컴포넌트 - TextInput

--- react_native_practice/App.js
+++ react_native_practice/App.js
@@ -1,13 +1,19 @@
-import React from "react";
-import { Image, View } from 'react-native';  
+import React, { useState } from "react";
+import { TextInput, View, Text } from 'react-native';  
 
 export default function App() {
+  const [text, setText] = useState('');
+
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-      <Image 
-      source={require('./assets/favicon.png')}
-      style={{ width: 200, height: 200 }}
-      resizeMode="contain" />
+    <View style={{ marginTop: 50, padding: 10 }}>
+      <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      placeholder="Type here"
+      onChangeText={text => setText(text)}
+      value={text}
+      />
+      <Text style={{ padding: 10, fontSize: 42 }}>
+        {text}
+      </Text>
     </View>
   )
 }
