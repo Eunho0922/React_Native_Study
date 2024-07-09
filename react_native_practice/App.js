5ae3b54b2d368e3f3336918de998e861758287cb
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-07-09 14:38:48 +0900

    React_Native :: 코어 컴포넌트 - FlatList

--- react_native_practice/App.js
+++ react_native_practice/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { ScrollView, Text } from 'react-native';  
+import { FlatList, Text, View } from 'react-native';  
+
+const DATA = [
+  { id: '1', title: 'Item 1' },
+  { id: '2', title: 'Item 2' },
+  { id: '3', title: 'Item 3' },
+];
+
+const Item = ({ title }) => (
+  <View style={{ padding: 20 }}>
+    <Text style={{ fontSize: 24 }}>{title}</Text>
+  </View>
+);
 
 export default function App() {
   return (
-    <ScrollView>
-      <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
-      <Text style={{ fontSize: 96 }}>If you like</Text>
-      <Text style={{ fontSize: 96 }}>Scrolling down</Text>
-      <Text style={{ fontSize: 96 }}>What's the best</Text>
-      <Text style={{ fontSize: 96 }}>Framework around?</Text>
-      <Text style={{ fontSize: 96 }}>React Native</Text>
-    </ScrollView>
-  )
+    <FlatList
+    data={DATA}
+    renderItem={({item}) => <Item title={item.title} />}
+    keyExtractor={item => item.id}
+    />
+  );
 }
