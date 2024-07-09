2c4a048ab84891c0e1b12f86cb6df0d35a62bf0f
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-07-09 09:41:56 +0900

    React :: li 컴포넌트 적용

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React from "react"
+
+import GoalList from "./componenets/GoalList"
 import './App.css';
 
-function App() {
-
-    // return React.createElement(
-    //   "h1",
-    //   { title: "This works!" },
-    //   "Hi, this is a React App!"
-    // );
-    return <h1 title='This wokrs!'>Hi, this is a React App!</h1>
+function App() { 
+    return (
+      <div className='course-goals'>
+        <h2>Course Goals</h2>
+        {/* <ul className="goal-list">
+        <li>Finish the Course</li>
+        <li>Learn all about the Course Main Topic</li>
+        <li>Help other students in the Course Q&A</li>
+        </ul>*/ }
+        <GoalList />
+      </div>
+    )
 }
 
 export default App;
---
+++ react_practice/src/componenets/GoalList.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import "./GoalList.css"
+
+const GoalList = () => {
+    return (
+        <ul className="goal-list">
+            <li>Finisth the Course</li>
+            <li>Learn all about the Course Main Topic</li>
+            <li>Help other students in the Course Q&A</li>
+        </ul>
+    )
+}
+
+export default GoalList;
